c0d7ac4d46ecf1e1734efb25c2ee2ac016234d29
z1x1ang <2360283295@qq.com>
2024-08-08 11:40:14 +0800

    webpage

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ fetch(`${experiment}.html`)
 .then(response => response.text())  // 解析响应为JSON(也可为blob等)
 .then(data => {
 document.getElementById('article-content').innerHTML=data;
+// 加载内容后，调用 MathJax 来处理页面上的新 LaTeX 代码
+MathJax.typeset();
 })    // 处理数据
 .catch(error => console.error('错误:', error));  // 处理错误
 }
